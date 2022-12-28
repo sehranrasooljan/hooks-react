7e5824e7635dc65c5be1dfa3b1b9db554fb50751
Umar mirza <mirzaumar089@gmail.com>
2022-12-28 12:57:37 +0530

    react hooks

--- src/App.js
+++ src/App.js
@@ -9,14 +9,36 @@ const App = () => {
   console.log("Inside App components")
 
    function inc(){
-      //  coins  = coins+1
-      setCoins(coins+1)
+  
+      if (coins<10)
+      {
+        setCoins(coins+1)
+      }
+      else{
+        alert("You can't have more than 10 coins")
+        
+      }
    }
 
+//    function dec(){
+//    if (coins>0)
+//    {
+//      setCoins(coins-1)
+//    }
+//    else
+//    {
+//       alert("You can't have less than 0 coins")
+//    }
+//  }
+ function reset(){
+  setCoins(coins=0)
+}
   return (
     <div>
       <h1>{coins}</h1>
       <button onClick={inc}>Increase</button>
+      <button onClick={()=>coins>1? setCoins(coins-1): alert("You can't have less than 1 coins")}>Decrease</button>  {/* This is the same as the function dec() above */}
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
